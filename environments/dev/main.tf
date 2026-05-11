4d64530e2ed6dbb4c4949dc18d01a129b33390a0
pawan <dhuriya.pawan@gmail.com>
2026-05-11 17:21:27 +0530

    TerraformACM

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,11 +1,15 @@
-# module "vpc" {
-#   source = "../../modules/vpc"
-# }
-
-module "acm" {
-  source = "../../modules/acm"
+terraform {
+  required_version = ">= 1.5.0"
 }
 
-# module "api_gateway" {
-#   source = "../../modules/api-gateway"
-# }
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+}
+
+module "api_gateway" {
+  source = "../../modules/api-gateway"
+}
