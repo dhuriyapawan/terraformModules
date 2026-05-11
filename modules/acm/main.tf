01d425f2efb3795b0d174d8ade9f020bb4265909
pawan <dhuriya.pawan@gmail.com>
2026-05-11 17:38:02 +0530

    TerraformACM

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -1,6 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_route53_zone" "selected" {
+  name         = var.zone_name
+  private_zone = false
+}
+
 resource "aws_acm_certificate" "cert" {
   domain_name       = var.domain_name
   validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_route53_record" "validation" {
@@ -8,16 +21,16 @@ resource "aws_route53_record" "validation" {
     for dvo in aws_acm_certificate.cert.domain_validation_options :
     dvo.domain_name => {
       name   = dvo.resource_record_name
-      record = dvo.resource_record_value
       type   = dvo.resource_record_type
+      record = dvo.resource_record_value
     }
   }
 
-  zone_id = var.zone_id
+  zone_id = data.aws_route53_zone.selected.zone_id
   name    = each.value.name
   type    = each.value.type
-  records = [each.value.record]
   ttl     = 60
+  records = [each.value.record]
 }
 
 resource "aws_acm_certificate_validation" "cert" {
--- modules/acm/variables.tf
+++ modules/acm/variables.tf
@@ -1,69 +1,7 @@
-variable "account_id" {
-  description = "The AWS Account ID to use for provisioning resources"
-  type        = string
-  default     = null
-}
-
-variable "region" {
-  description = "Region where the resource will be managed. Defaults to the region set in the provider configuration"
-  type        = string
-  default     = null
-}
-
-variable "deployment_profile" {
-  type        = string
-  default     = "dev"
-  description = "AWS Profile to use in Terraform Providers"
-}
-
-variable "environment" {
-  type        = string
-  default     = "dev"
-  description = "Environment to deploy"
-}
-
-variable "prefix_name" {
-  description = "This is the the prefix name to use for naming resources"
-  type        = string
-  default     = "dev-use2"
-}
-
-variable "tf_deployment_version" {
-  description = "This is the version for terraform github action"
-  type        = string 
-}
-
-variable "tags" {
-  description = "Common tags applied to all resources"
-  type        = map(string)
-  default     = {}
-}
-
-
 variable "domain_name" {
-  description = "Domain name for ACM"
   type = string
 }
 
-variable "subject_alternative_names" {
-  description = "Alternative names for ACM"
-}
-
-variable "zone_id" {
-  description = "Zone ID"
+variable "zone_name" {
   type = string
 }
-
-variable "validation_method" {
-  description = "Validation method for ACM"
-  type = string
-  default = "DNS"
-}
-
-variable "ttl" {
-  description = "Time to live for route53 record"
-  type = number
-  default = 60
-  
-}
-
